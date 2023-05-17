2928c8b83fdaea1fabff242fe275ccf896467db5
sanjanamunduri <133853176+sanjanamunduri@users.noreply.github.com>
2023-05-17 16:48:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's new healthy Diner')
+streamlit.title("My Mom's new healthy Diner")
 streamlit.header('Breakfast favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
