b3fd4521037d2d3dfe115abd59eadaeb3241098a
sanjanamunduri <133853176+sanjanamunduri@users.noreply.github.com>
2023-05-17 16:30:37 +0530

    Update streamlit_app.py
    Added header and options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idly,Podi Idly,Uthapam)
+Streamlit.text('Dosa - All varities)               
