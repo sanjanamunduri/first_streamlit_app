56c0285cb6387965f50dfafadb87eedbb8c6f729
sanjanamunduri <133853176+sanjanamunduri@users.noreply.github.com>
2023-05-17 16:36:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Idly,Podi Idly,Uthapam')
-streamlit.text('Idly,Podi Idly,Uthapam')
+streamlit.text('Dosa')
 streamlit.text('Poori')
