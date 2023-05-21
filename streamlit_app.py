4bd032534b8145d4f310d2b28d4f6ad1e85a5a33
sanjanamunduri <133853176+sanjanamunduri@users.noreply.github.com>
2023-05-21 11:57:36 +0530

    Update streamlit_app.py
    nested statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,22 +21,31 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+## structuring with if else nested
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Banana')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Banana')
+   if not fruit_choice:
+        streamllit.error("Please select a fruit of your choice")
+   else:   
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  sreamlit.error()
+#streamlit.write('The user entered ', fruit_choice)
 
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 ##streamlit.text(fruityvice_response.json())
 
 
 
 # import pandas
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
 
 ## Organising , dont run anyting past this
 streamlit.stop()
